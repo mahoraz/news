4e6fc509b7f39e0cf2f53dc1b7d92bdbd694a1f3
Jaanus <jaanus@stagnationlab.com>
2023-04-06 11:57:09 +0300

    added basic typerorm config, Artile model, routes

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,6 @@ import http from 'http';
 import app from './app';
 import {config} from './config';
 
-class News {
-    articleName: String;
-    constructor(name: String){
-        this.articleName = name;
-    }
-
-    getInfo(){
-        return `Article ${this.articleName}`;
-    }
-
-    save(){
-
-    }
-}
-
-
 
 app.listen(config.server.port, config.server.host , () => {
     console.log(`Server running on ${config.server.host}:${config.server.port}`);
